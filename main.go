3284b0687ae7a8a7cb0089e01ac7eab9ab99d04e
WincerChan <wincerchan@gmail.com>
2018-07-14 15:33:43 +0800

    Refact: remove mux router

--- main.go
+++ main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	"encoding/json"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// AMOUNT is Number of databases
-var AMOUNT int64
-var db *sql.DB
-var err error
-
-// Config is pwd and user
-type Config struct {
-	User     string
-	Password string
-}
-
-func initDatabase() {
-	file, _ := os.Open("config.json")
-	decoder := json.NewDecoder(file)
-	config := Config{}
-	err := decoder.Decode(&config)
-	url := fmt.Sprintf("%s:%s@/hitokoto?charset=utf8", config.User, config.Password)
-	db, err = sql.Open("mysql", url)
-	checkErr(err)
-	err1 := db.QueryRow("SELECT COUNT(id) FROM main;").Scan(&AMOUNT)
-	checkErr(err1)
-}
-
 func main() {
-	initDatabase()
-	router := NewRouter()
+	initHitokotoDB()
 
+	http.HandleFunc("/hitokoto/v2/", Hitokoto)
 	log.Println("listening in 520 port.")
-
-	log.Fatal(http.ListenAndServe(":8020", router))
-}
-
-func checkErr(err error) {
-	switch {
-	case err == sql.ErrNoRows:
-		log.Printf(" None Query ")
-	case err != nil:
-		log.Fatal(err)
-	}
+	err := http.ListenAndServe(":8020", nil)
+	checkErr(err)
 }
