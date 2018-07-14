7f6a7622d6051ae93cf723de77d05d86d75d004c
WincerChan <wincerchan@gmail.com>
2018-07-14 22:28:04 +0800

    Add Header for param

--- hitokoto.go
+++ hitokoto.go
@@ -32,6 +32,12 @@ func Hitokoto(w http.ResponseWriter, r *http.Request) {
 	length := r.Form.Get("length")
 	// if url param have callback then will ignore encode
 	callback := r.Form.Get("callback")
+	charset := r.Form.Get("charset")
+	if charset == "gbk" {
+	} else {
+		charset = "utf-8"
+	}
+
 	if length != "" {
 		err1 := db.QueryRow("SELECT hitokoto, source FROM main WHERE LENGTH(hitokoto) < ? ORDER BY RAND() LiMIT 1;", length).Scan(&hito, &source)
 		if err1 != nil {
@@ -48,11 +54,12 @@ func Hitokoto(w http.ResponseWriter, r *http.Request) {
 	if callback != "" {
 		hs := &Hit{hito, source}
 		fmtJSON, _ := json.Marshal(hs)
+		w.Header().Set("Content-Type", "text/javascript; charset="+charset)
 		fmt.Fprintf(w, "%s(%s);", callback, fmtJSON)
 	} else {
 		if encode == "js" {
 			content = fmt.Sprintf("%s\\n——「%s」", hito, source)
-			w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+			w.Header().Set("Content-Type", "text/javascript; charset="+charset)
 			fmt.Fprintf(w, "var hitokoto=\"%s\";var dom=document.querySelector('.hitokoto');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;", content)
 		} else if encode == "json" {
 			hs := &Hit{
@@ -60,11 +67,13 @@ func Hitokoto(w http.ResponseWriter, r *http.Request) {
 				source,
 			}
 			fmtJSON, _ := json.Marshal(hs)
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.Header().Set("Content-Type", "application/json; charset="+charset)
 			fmt.Fprintf(w, "%s", string(fmtJSON))
 		} else if encode == "text" {
+			w.Header().Set("Content-Type", "text/plain; charset="+charset)
 			fmt.Fprintf(w, "%s", hito)
 		} else {
+			w.Header().Set("Content-Type", "text/plain; charset="+charset)
 			content = fmt.Sprintf("%s——「%s」", hito, source)
 			fmt.Fprintf(w, "%s", content)
 		}
