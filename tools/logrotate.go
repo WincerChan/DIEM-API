60d40ed56ea890a9a15bb2705ce69de45545df9c
WincerChan <WincerChan@gmail.com>
2018-11-30 06:01:00 +0100

    :construction: Remove CopyFile function

--- tools/logrotate.go
+++ tools/logrotate.go
@@ -2,8 +2,7 @@ package tools
 
 import (
 	"io"
-	"os"
-	"strings"
+	"path"
 	"time"
 )
 
@@ -16,30 +15,27 @@ const (
 )
 
 type Logger struct {
-	Writer io.Writer
-	Ltype  string
+	Writer   io.Writer
+	Ltype    string
+	fullName string
 }
 
-func CopyFile(srcFile, destFile string) error {
-	file, err := os.Open(srcFile)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	dest, err := os.Create(destFile)
-	if err != nil {
-		return err
-	}
-	defer dest.Close()
-	_, err = io.Copy(dest, file)
-	return err
+func (l *Logger) createFile() {
+	l.fullName = path.Join(logpath, l.Ltype, l.Ltype+".log")
+	l.Writer = FileCreator.New(l.fullName)
 }
 
 func NewLogger(ltype string) *Logger {
 	llog := new(Logger)
-	llog.Writer = FileCreator.New(ltype, false)
 	llog.Ltype = ltype
+	llog.createFile()
+
 	return llog
+}
+
+func (l *Logger) doRotate(now time.Time) {
+	CopyFile(l.fullName, l.fullName+now.Format("2006-01-02"))
+	l.Writer = FileCreator.New(l.fullName)
 }
 
 func (l *Logger) rename(now time.Time) {
@@ -49,10 +45,7 @@ func (l *Logger) rename(now time.Time) {
 
 	t := time.NewTimer(restHour + restMinute + restSecond)
 	<-t.C
-	CopyFile(strings.Join([]string{logpath, l.Ltype, l.Ltype + ".log"}, "/"),
-		strings.Join([]string{logpath, l.Ltype, l.Ltype + now.Format("2006-01-02")}, "/"))
-	l.Writer = FileCreator.New(l.Ltype, true)
-	return
+	l.doRotate(now)
 }
 
 func (l *Logger) Rotate() {
