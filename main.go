2c72f0d24f11858e834e267c5be416e85fb95c65
WincerChan <WincerChan@gmail.com>
2019-05-15 13:30:12 +0800

    :fire: Add a comment

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	http.HandleFunc("/hitokoto/v2/", Hitokoto)
 	http.HandleFunc("/hitokoto/get", Redirect301)
-	http.HandleFunc("/thinking/v1/", HandleThinkReq)
+	// http.HandleFunc("/thinking/v1/", HandleThinkReq)
 	http.HandleFunc("/", Home)
 	log.Println("listening in 520 port.")
 	err := http.ListenAndServe(":5200", nil)
