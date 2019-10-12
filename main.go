7e0af6f864c6377e092a9a1b977e3cf734e5853d
WincerChan <WincerChan@gmail.com>
2019-10-12 06:00:00 +0100

    :bug: Remove a url

--- main.go
+++ main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	configPath := flag.String("config", "config.yaml", "Config file.")
-	if len(os.Args) <= 1 {
-		fmt.Println("No enough arguments.")
-		fmt.Println("Use: ./server [prod|dev]")
-		os.Exit(1)
-	}
-	switch os.Args[1] {
-	case "prod":
-		initLogFile()
-	case "dev":
-	}
 	initConfig(*configPath)
 	initHitokotoDB()
 	initRedis()
