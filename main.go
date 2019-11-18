940271dade94b507cab92bb6e2246dd7c754086a
WincerChan <WincerChan@gmail.com>
2019-11-18 13:02:24 +0800

    :wrench: Move initial database to init funciton

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"net/http"
 )
 
-var configPath = flag.String("config", "config.yaml", "Config file.")
-
-func main() {
+func init() {
+	configPath := flag.String(
+		"config",
+		"config.yaml",
+		"Database Config file.")
 	flag.Parse()
-	
 	initConfig(*configPath)
 	initHitokotoDB()
 	initRedis()
+}
 
+func main() {
 	log.Println("listening in " + config.ListenPort + "port.")
 	http.HandleFunc("/hitokoto/v2/", Hitokoto)
 
