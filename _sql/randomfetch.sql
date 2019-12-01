52b79901a43b48fb60f9f031fe38c8015d52640f
WincerChan <WincerChan@gmail.com>
2019-12-01 06:01:00 +0100

    :construction: Rename sql directory

---
+++ _sql/randomfetch.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION public.randomfetch(len integer)
+ RETURNS TABLE(data jsonb)
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  amount int;
+  lengths smallint;
+BEGIN
+  IF len < 4 THEN
+    amount := (SELECT reltuples::bigint FROM pg_catalog.pg_class WHERE relname = 'hitokoto');
+    lengths := 32767;
+  ELSE
+    amount := (SELECT count(id) FROM hitokoto WHERE length < len);
+    lengths := len;
+  END IF;
+  RETURN QUERY
+    SELECT info FROM hitokoto WHERE length < lengths LIMIT 1 OFFSET (SELECT FLOOR(RANDOM() * amount));
+END
+$function$;
