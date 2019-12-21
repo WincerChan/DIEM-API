0fed333b5738989045357312867f867be1db0553
WincerChan <WincerChan@gmail.com>
2019-12-21 06:06:00 +0100

    :hammer: Remove test function

--- main.go
+++ main.go
@@ -9,27 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func pong(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
-func pang(c *gin.Context) {
-	k := make([]int, 0)
-	_ = k[0]
-	c.JSON(200, gin.H{
-		"message": "pang",
-	})
-}
-
 func main() {
 	r := gin.New()
 	r.Use(L.Log)
 	r.Use(R.Recover)
 	r.Use(C.Limiting)
-	r.GET("/pong", pong)
-	r.GET("/pang", pang)
 	r.GET("/hitokoto/v2/", S.Hitokoto)
 	r.Run()
 }
