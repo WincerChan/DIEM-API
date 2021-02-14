7549c3933577a05a481f9a753f5b2e15fba8a0fa
WincerChan <WincerChan@gmail.com>
2021-02-14 22:40:03 +0800

    add models package

--- models/googleanalytics/model.go
+++ models/googleanalytics/model.go
@@ -1,32 +1,32 @@
-package gaviews
+package googleanalytics
 
 import (
-	C "DIEM-API/config"
 	T "DIEM-API/tools"
+
 	gar "google.golang.org/api/analyticsreporting/v4"
 )
 
-// gaviews url params
+var (
+	GAViewID string
+)
+
 type Params struct {
 	Prefix string `form:"prefix"`
 }
 
-// gaviews response format
 type ReportResponse struct {
 	Details []accessInfo `json:"details"`
 	Total   int          `json:"total"`
 }
 
-//gaviews every page access info format
 type accessInfo struct {
 	Path  string `json:"path"`
 	Count int    `json:"count"`
 }
 
-// the google analytics request report template
-func GetPathReport(p *Params, viewID string) *gar.ReportRequest {
+func ConstructReport(p *Params) *gar.ReportRequest {
 	reportParams := new(gar.ReportRequest)
-	reportParams.ViewId = viewID
+	reportParams.ViewId = GAViewID
 	reportParams.DateRanges = []*gar.DateRange{&gar.DateRange{StartDate: "2017-06-18", EndDate: "today"}}
 	reportParams.Metrics = []*gar.Metric{&gar.Metric{Expression: "ga:pageviews"}}
 	reportParams.Dimensions = []*gar.Dimension{&gar.Dimension{Name: "ga:pagePath"}}
@@ -43,7 +43,6 @@ func GetPathReport(p *Params, viewID string) *gar.ReportRequest {
 	return reportParams
 }
 
-// remove some field that don't need
 func SimplifiedResponse(response *gar.GetReportsResponse) (rr ReportResponse) {
 	rr = *new(ReportResponse)
 	for _, report := range response.Reports {
@@ -54,16 +53,4 @@ func SimplifiedResponse(response *gar.GetReportsResponse) (rr ReportResponse) {
 		}
 	}
 	return
-}
-
-func GetPageViews(p *Params, pageView *ReportResponse) {
-	report := GetPathReport(p, C.GAViewID)
-	req := &gar.GetReportsRequest{
-		ReportRequests: []*gar.ReportRequest{
-			report,
-		},
-	}
-	resp, err := C.AnalyticsReportingService.Reports.BatchGet(req).Do()
-	T.CheckException(err, "Failed to get analytics report.")
-	*pageView = SimplifiedResponse(resp)
 }
---
+++ models/interface.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	G "DIEM-API/models/googleanalytics"
+	H "DIEM-API/models/hitokoto"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+func InitHitokoto(tx *bolt.Tx) error {
+	H.HitokotoMapping = make(map[int]int)
+	return H.ScanRecordLength(tx)
+}
+
+func InitGoogleAnalytics(viewID string) {
+	InitGACredential()
+	G.GAViewID = viewID
+}
