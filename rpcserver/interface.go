f3c6172d49e0b5a32ec6ec3160e63eb99fd57eb7
WincerChan <WincerChan@gmail.com>
2021-04-18 15:18:43 +0800

    feat: support compound params

--- rpcserver/interface.go
+++ rpcserver/interface.go
@@ -4,8 +4,19 @@ func Choke(key string, total int, speed float64, p *Pool) []interface{} {
 	rpc := new(RPCEncode)
 	rpc.encodeAtom("choke")
 	rpc.encodeString(key)
-	rpc.encodeInteger(uint32(total))
+	rpc.encodeInteger(total)
 	rpc.encodeFloat(speed)
+	conn := p.Get()
+	defer p.Put(conn)
+	return rpc.execute(conn)
+}
+
+func Search(pages, ranges []int, terms, q []string, p *Pool) []interface{} {
+	rpc := new(RPCEncode)
+	rpc.encodeIntegerList(pages)
+	rpc.encodeIntegerList(ranges)
+	rpc.encodeStringList(terms)
+	rpc.encodeStringList(q)
 	conn := p.Get()
 	defer p.Put(conn)
 	return rpc.execute(conn)
