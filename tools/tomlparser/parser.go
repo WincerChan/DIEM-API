fd13c5a17cc35d4d0dbf81c68a99c597426bdb96
WincerChan <WincerChan@gmail.com>
2021-12-24 20:00:49 +0800

    pass path as args

--- tools/tomlparser/parser.go
+++ tools/tomlparser/parser.go
@@ -2,28 +2,15 @@ package tomlparser
 
 import (
 	T "DIEM-API/tools"
-	"os"
 	"path/filepath"
 
 	"github.com/pelletier/go-toml"
 )
 
-const filename = "diem.toml"
-
 var Config *toml.Tree
 
-func checkConfigExists(name string) string {
-	path := os.Getenv("CONFIG")
-	if path == "" {
-		println("Please set environment variable `CONFIG`")
-		os.Exit(1)
-	}
-	return filepath.Join(path, name)
-}
-
-func LoadTOML() {
+func LoadTOML(path string) {
 	var err error
-	path := checkConfigExists(filename)
 	Config, err = toml.LoadFile(path)
 	T.CheckFatalError(err, false)
 }
